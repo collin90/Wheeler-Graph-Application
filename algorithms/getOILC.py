565ab253265b223ff2cd740e1c84826b8d6ec157
Collin Hughes <chughes@pcx.procentrixinc.com>
2022-11-04 12:16:07 -0400

    made OILC download file much more compact

--- algorithms/getOILC.py
+++ algorithms/getOILC.py
@@ -1,3 +1,4 @@
+from bitstring import BitArray
 
 def getOILC (nodes, edges): 
     if(len(nodes) == 0): return {'O': [], 'I': [], 'L': [], 'C': []}
@@ -37,6 +38,54 @@ def getOILC (nodes, edges):
         for o_e in out_edges:
             L += o_e['label']
 
+    #now that we have O, I, and L, we'll just compress them down a little to make the overall download file very small.
+    O_compressed = ''
+    i = 0
+    while (i < len(O)):
+        if(i < len(O)-8): current_bitstr = O[i:i+8]
+        else: current_bitstr = O[i:len(O)]
+        O_compressed += str(int(current_bitstr,2)) + 'x'
+        count = 1
+        while (True): 
+            i += 8
+            if(i < len(O)-8 and O[i:i+8] == current_bitstr):
+                count += 1
+            else :
+                O_compressed += str(count) + ','
+                break
+
+    I_compressed = ''
+    i = 0
+    while (i < len(I)):
+        if(i < len(I)-8): current_bitstr = I[i:i+8]
+        else: current_bitstr = I[i:len(I)]
+        I_compressed += str(int(current_bitstr,2)) + 'x'
+        count = 1
+        while (True): 
+            i += 8
+            if(i < len(I)-8 and I[i:i+8] == current_bitstr):
+                count += 1
+            else :
+                I_compressed += str(count) + ','
+                break
+
+    
+    L_compressed = ''
+    i = 0
+    while (i<len(L)):
+        cur = L[i]
+        L_compressed += cur
+        count = 1
+        while (True):
+            i += 1
+            if (i < len(L) and L[i] == cur):
+                count += 1
+            else :
+                L_compressed += str(count)
+                break
+
+
+
     # C   (array containing |A| elements, where each element i is the number of edges with label <= A[i])
     # example: if edges are labeled with: a,a,a,a,b,b,b,c,c,d
     #          then C would be: [4,7,9,10]
@@ -52,4 +101,4 @@ def getOILC (nodes, edges):
             if(edge['label'] <= a): C[i] += 1
 
             
-    return {'O': O, 'I': I, 'L': L, 'C': C}
+    return {'O': O_compressed, 'I': I_compressed, 'L': L_compressed, 'C': C}
