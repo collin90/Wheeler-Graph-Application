3cd476d103d6b1d64b5ed29c4065c30c3a9f087f
Joanna Bi <biyjoanna@gmail.com>
2022-11-09 18:53:07 -0500

    added interactive graph

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -2,20 +2,54 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import MathJax from "react-mathjax";
+import Graph from "../components/Graph";
+import InteractiveGraph from "../components/InteractiveGraph";
+import { MarkerType } from 'reactflow';
+
+const nodes2 = [
+    { id: 'node1', style: { width: 30, height: 30, fontsize: 10 }, position: { x: 100, y: 50 } },
+    { id: 'node2', style: { width: 30, height: 30, fontsize: 10 }, position: { x: 175, y: 50 } },
+    { id: 'node3', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '30' }, position: { x: 100, y: 200 } },
+    { id: 'node4', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '40' }, position: { x: 175, y: 200 } },
+    { id: 'node5', style: { width: 30, height: 30, fontsize: 10 }, position: { x: 350, y: 50 } },
+    { id: 'node6', style: { width: 30, height: 30, fontsize: 10 }, position: { x: 425, y: 50 } },
+    { id: 'node7', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '10' }, position: { x: 350, y: 200 } },
+]
+
+const edges2 = [
+    { id: 'edge1', animated: true, source: 'node1', target: 'node3', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge2', animated: true, source: 'node2', target: 'node4', label: 'B', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge3', animated: true, source: 'node5', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#FF0000', }, style: { strokeWidth: 2, stroke: '#FF0000', } },
+    { id: 'edge4', animated: true, source: 'node6', target: 'node7', label: 'B', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#FF0000', }, style: { strokeWidth: 2, stroke: '#FF0000', } },
+]
+
+const nodes3 = [
+    { id: 'node1', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '10' }, position: { x: 100, y: 50 } },
+    { id: 'node2', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '30' }, position: { x: 175, y: 50 } },
+    { id: 'node3', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '20' }, position: { x: 100, y: 200 } },
+    { id: 'node4', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '40' }, position: { x: 175, y: 200 } },
+    { id: 'node5', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '10' }, position: { x: 350, y: 50 } },
+    { id: 'node6', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '30' }, position: { x: 425, y: 50 } },
+    { id: 'node7', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '40' }, position: { x: 350, y: 200 } },
+]
+
+const edges3 = [
+    { id: 'edge1', animated: true, source: 'node1', target: 'node3', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge2', animated: true, source: 'node2', target: 'node4', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge3', animated: true, source: 'node5', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge4', animated: true, source: 'node6', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+]
 
 function Tutorial() {
     return (
         <Container>
             <MathJax.Provider>
-                <Box py={3}>
-                    <Typography variant="h5"> Wheeler Graph Tutorial </Typography>
+                <Box pt={3}>
+                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Wheeler Graph Definition </Typography>
                 </Box>
                 <Box>
-                    <Typography variant="h6"> What is a Wheeler Graph? </Typography>
-                </Box>
-                <Box>
-                    <Typography variant="body1"> A directed edge-labeled graph, where each label is from a totally-ordered alphabet, is a Wheeler Graph if </Typography>
-                    <Typography variant="body1"> The nodes can be ordered such that: </Typography>
+                    <Typography variant="body1"> A directed edge-labeled graph, where each label is from a totally-ordered alphabet, is a <b>Wheeler Graph</b> if: </Typography>
+                    <Typography variant="body1"> The nodes can be ordered such that, </Typography>
                     <ol>
                         <li>Nodes with in-degree 0 come before nodes with positive in-degree</li>
                         <Typography variant="body1" ml={4}> Nodes without an arrow pointing towards them must come first </Typography>
@@ -23,7 +57,7 @@ function Tutorial() {
                     <Typography variant="body1">
                         And for all pairs of edges
                         <MathJax.Node inline formula={`\\: e = (u,v), e' = (u', v')\\:`} />
-                        (labeled a and a' respectively):
+                        (labeled a and a' respectively),
                     </Typography>
                     <ol start="2">
                         <li>
@@ -31,19 +65,48 @@ function Tutorial() {
                             <Typography variant="body1" ml={4}>If an edge labeled a is alphabetically less than an edge labeled a', then that the lesser edge's destination should be less than the greater edge's destination. </Typography>
                             <Typography variant="body1" ml={6}>This also creates the corollary that all edges entering the same node have the same label since a node cannot have two incoming edges with different labels </Typography>
                         </li>
+                        <div style={{ width: 500, height: 300 }}>
+                            <Graph nodes={nodes2} edges={edges2} fitView={true} />
+                        </div>
                         <li>
                             <MathJax.Node inline formula={`(a = a') \\land (u < u') \\Longrightarrow v \\leq v'`} />
                             <Typography variant="body1" ml={4}>If an edge labeled a is alphabetically equal to an edge labeled a' and the source of a is less than the source of a', then the destination of a must be less than or equal to the destination of a'. </Typography>
                         </li>
+                        <div style={{ width: 500, height: 300 }}>
+                            <Graph nodes={nodes3} edges={edges3} fitView={true} />
+                        </div>
                     </ol>
                 </Box>
+                <Box pt={3}>
+                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Wheeler Property </Typography>
+                </Box>
+                <Box pt={3}>
+                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Try for Yourself! </Typography>
+                    <Typography variant="body1" ml={4}>
+                        1. Create a new node by dragging an edge from any existing node <br></br>
+                        2. Change the label of an edge by clicking on it <br></br>
+                        3. Remove a node and its neighboring edges by clicking on it <br></br>
+                        4. Remove an edge by clicking it when it is already labeled 'd' <br></br>
+                    </Typography>
+                </Box>
+                <Box py={2} sx={{ border: 3}} mb={3}>
+                    <div style={{ width: 'auto', height: window.innerHeight / 1.5 }}>
+                        <InteractiveGraph />
+                    </div>
+                </Box>
+                <Box pt={3}>
+                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Why are Wheeler Graphs important? </Typography>
+                </Box>
                 <Box>
-                    <Typography variant="h6"> Why are Wheeler Graphs important? </Typography>
+                    <Typography variant="body1"> Wheeler graphs are desirable because they have the <b>path coherence</b> property, meaning that for any string, we can order nodes consecutively from the initial state to process that string. This property allows for the graph to be stored compactly </Typography>
+                    <br></br>
+                    <Typography variant="body1">
+                        Given a graph G = (V,E), where V = set of vertices and E = set of edges, and σ is the size of the alphabet. |V| = n, |E|= e. Then, the graph may be stored in
+                        <MathJax.Node inline formula={` \\: 2(e + n)+ e\\log\\sigma + \\sigma\\log e + o(n+e\\log\\sigma) \\:`} />
+                        bits.
+                    </Typography>
                 </Box>
             </MathJax.Provider>
         </Container>
-
     )
-
-
 } export default Tutorial;
