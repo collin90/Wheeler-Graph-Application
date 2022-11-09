019f61a4f50680cf599ac0a81ce025c701649e29
Collin Hughes <chughes@pcx.procentrixinc.com>
2022-11-09 16:03:29 -0500

    pattern match now has example of interactivegraph

--- src/pages/PatternMatch.jsx
+++ src/pages/PatternMatch.jsx
@@ -3,11 +3,18 @@ import Box from "@mui/material/Box";
 
 function PatternMatch () {
     return (
-        <Box py={2} sx={{ border: 3}} mb={3}>
-            <div style={{width:'auto', height:window.innerHeight/1.5}}>
-                <InteractiveGraph />
+        <>
+            <div>
+                1. Create a new node by dragging an edge from any existing node <br></br>
+                2. Change the label of an edge by clicking on it <br></br>
+                3. Remove a node and its neighboring edges by clicking on it <br></br>
             </div>
-        </Box>
+            <Box py={2} sx={{ border: 3}} mb={3}>
+                <div style={{width:'auto', height:window.innerHeight/1.5}}>
+                    <InteractiveGraph />
+                </div>
+            </Box>
+        </>
     );
 
 } export default PatternMatch;
