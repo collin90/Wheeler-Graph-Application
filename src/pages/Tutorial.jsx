87239a42d0f9315ba79108c9e5ff00e196486ce9
Joanna Bi <biyjoanna@gmail.com>
2022-11-09 20:04:17 -0500

    Complete draft of tutorial

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -40,15 +40,40 @@ const edges3 = [
     { id: 'edge4', animated: true, source: 'node6', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
 ]
 
+const nodesProp = [
+    { id: 'node1', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '1' }, position: { x: 100, y: 50 } },
+    { id: 'node2', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '2' }, position: { x: 175, y: 50 } },
+    { id: 'node3', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '3' }, position: { x: 100, y: 200 } },
+    { id: 'node4', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '4' }, position: { x: 175, y: 200 } },
+    { id: 'node5', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '5' }, position: { x: 350, y: 50 } },
+    { id: 'node6', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '6' }, position: { x: 425, y: 50 } },
+    { id: 'node7', style: { width: 30, height: 30, fontsize: 10 }, data: { label: '7' }, position: { x: 350, y: 200 } },
+]
+
+const edgesProp = [
+    { id: 'edge1', animated: true, source: 'node1', target: 'node3', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge2', animated: true, source: 'node2', target: 'node4', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge3', animated: true, source: 'node5', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+    { id: 'edge4', animated: true, source: 'node6', target: 'node7', label: 'A', markerEnd: { type: MarkerType.ArrowClosed, width: 8, height: 8, color: '#00FF00', }, style: { strokeWidth: 2, stroke: '#00FF00', } },
+]
+
 function Tutorial() {
     return (
         <Container>
             <MathJax.Provider>
                 <Box pt={3}>
+                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Why Wheeler? </Typography>
+                    <Typography variant="body1" ml={4}>
+                        The Burrows-Wheeler Transform is an algorithm that compresses a string. Due to properties like reversibility, it is very useful for many applications.
+                        It originally only compresses a single string, but there now there are variations for other data structures like sets of strings and de Bruijn graphs. 
+                        The Wheeler graph is a framework that includes these variations. 
+                    </Typography>
+                </Box>
+                <Box pt={3}>
                     <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Wheeler Graph Definition </Typography>
                 </Box>
                 <Box>
-                    <Typography variant="body1"> A directed edge-labeled graph, where each label is from a totally-ordered alphabet, is a <b>Wheeler Graph</b> if: </Typography>
+                    <Typography variant="body1"> A <b>Wheeler Graph</b> is a directed edge-labeled graph, where each label is from a totally-ordered alphabet and following are true. </Typography>
                     <Typography variant="body1"> The nodes can be ordered such that, </Typography>
                     <ol>
                         <li>Nodes with in-degree 0 come before nodes with positive in-degree</li>
@@ -79,6 +104,19 @@ function Tutorial() {
                 </Box>
                 <Box pt={3}>
                     <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Wheeler Property </Typography>
+                    <Typography variant="body1">
+                        Wheeler graphs have a property called <b>path coherence.</b>
+                    </Typography>
+                    <Typography variant="body1" ml={4} py={2}>
+                        "G is path coherent if there is a total order of the nodes such that for any consecutive range [<i>i, j</i>] of nodes and string α, the nodes reachable from those in [<i>i, j</i>] in |α| steps by following edges whose labels for α when concatenated, themselves form a consecutive range" (Gagie 2017)
+                    </Typography>
+                    <Typography variant="body1">
+                        This means that when matching a string, you can start from the shortest suffix and all edges that match that character 
+                        reach nodes with consecutive labels. Then, going from those nodes along the edge with the next character, you will reach
+                        at nodes that are also consecutive. 
+                        <br></br><br></br>
+                        Because of this property, the Wheeler graph can be stored compactly and process strings quickly
+                    </Typography>
                 </Box>
                 <Box pt={3}>
                     <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Try for Yourself! </Typography>
@@ -89,22 +127,10 @@ function Tutorial() {
                         4. Remove an edge by clicking it when it is already labeled 'd' <br></br>
                     </Typography>
                 </Box>
-                <Box py={2} sx={{ border: 3}} mb={3}>
+                <Box py={2} sx={{ border: 3 }} mb={3}>
                     <div style={{ width: 'auto', height: window.innerHeight / 1.5 }}>
                         <InteractiveGraph />
                     </div>
-                </Box>
-                <Box pt={3}>
-                    <Typography variant="h4" sx={{ fontWeight: 'medium' }}> Why are Wheeler Graphs important? </Typography>
-                </Box>
-                <Box>
-                    <Typography variant="body1"> Wheeler graphs are desirable because they have the <b>path coherence</b> property, meaning that for any string, we can order nodes consecutively from the initial state to process that string. This property allows for the graph to be stored compactly </Typography>
-                    <br></br>
-                    <Typography variant="body1">
-                        Given a graph G = (V,E), where V = set of vertices and E = set of edges, and σ is the size of the alphabet. |V| = n, |E|= e. Then, the graph may be stored in
-                        <MathJax.Node inline formula={` \\: 2(e + n)+ e\\log\\sigma + \\sigma\\log e + o(n+e\\log\\sigma) \\:`} />
-                        bits.
-                    </Typography>
                 </Box>
             </MathJax.Provider>
         </Container>
