8e13d4c8e615a09237848fd83fac6e02f4129adc
Collin Hughes <chughes@pcx.procentrixinc.com>
2022-11-13 15:44:39 -0500

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/collin90/Wheeler-Graph-Application/"
+  base: "/Wheeler-Graph-Application/"
 })
