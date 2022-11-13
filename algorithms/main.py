c97d6e6987cd001fa0277cd97388a30d816def88
Collin Hughes <chughes@pcx.procentrixinc.com>
2022-11-13 15:29:54 -0500

    max_iters is 1,000,000,000

--- algorithms/main.py
+++ algorithms/main.py
@@ -42,7 +42,7 @@ def checkWheeler():
 
 @app.route('/findOrdering', methods=['POST'])
 def findOrdering():
-    r = find_ordering({'nodes': request.json['nodes'], 'edges': request.json['edges']}, MAX_ITERATIONS=1000000000000000000)
+    r = find_ordering({'nodes': request.json['nodes'], 'edges': request.json['edges']}, MAX_ITERATIONS=1000000000)
     return jsonify({"graph": str(r['ordering']), "message": r['message']})
 
 @app.route("/")
