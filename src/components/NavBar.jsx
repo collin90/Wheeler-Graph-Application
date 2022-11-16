67850d7e9e54111fd1004339db3ca9feae878fe2
Collin Hughes <chughes@pcx.procentrixinc.com>
2022-11-15 19:29:34 -0500

    merge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,35 +26,35 @@ function NavBar () {
             </ul>
         </nav>
     );
-    const [value, setValue] = React.useState('Wheeler Graphs');
-    const navigate = useNavigate();
+    //const [value, setValue] = React.useState('Wheeler Graphs');
+    //const navigate = useNavigate();
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-      if(newValue == 'Wheeler Graphs') navigate('/');
-      else if (newValue == 'Tutorial') navigate('/tutorial');
-      else if (newValue == 'Generate/Visualize') navigate('/visualize');
-      else if (newValue == 'Pattern Match') navigate('/patternmatch');
-      else if (newValue == 'Wheeler Property') navigate('/wheelerproperty')
-    };
+    //const handleChange = (event, newValue) => {
+      //setValue(newValue);
+      //if(newValue == 'Wheeler Graphs') navigate('/');
+      //else if (newValue == 'Tutorial') navigate('/tutorial');
+      //else if (newValue == 'Generate/Visualize') navigate('/visualize');
+      //else if (newValue == 'Pattern Match') navigate('/patternmatch');
+      //else if (newValue == 'Wheeler Property') navigate('/wheelerproperty')
+    //};
   
-    return (
-      <Box mt={1} bgcolor='light-blue' sx={{ width: '100%', justifyContent: "flex", display: 'flex', fontSize: '38px', fontWeight: 'bolder'}}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="primary"
-          sx={{'backgroundColor':'#EDF1FF', width: '100%'}}
-          indicatorColor="primary"
-          aria-label="secondary tabs example"
-        >
-            <Tab value="Wheeler Graphs" label="Wheeler Graphs"></Tab>
-            <Tab value="Tutorial" label="Tutorial"></Tab>
-            <Tab value="Generate/Visualize" label="Generate/Visualize" ></Tab>
-            <Tab value="Pattern Match" label="Pattern Matching" ></Tab>
-            <Tab value="Wheeler Property" label="Find Ordering" ></Tab>
-        </Tabs>
-      </Box>
-    ); 
+    //return (
+     // <Box mt={1} bgcolor='light-blue' sx={{ width: '100%', justifyContent: "flex", display: 'flex', fontSize: '38px', fontWeight: 'bolder'}}>
+     //   <Tabs
+     //     value={value}
+     //     onChange={handleChange}
+     //     textColor="primary"
+     //     sx={{'backgroundColor':'#EDF1FF', width: '100%'}}
+     //     indicatorColor="primary"
+      //    aria-label="secondary tabs example"
+      //  >
+      //      <Tab value="Wheeler Graphs" label="Wheeler Graphs"></Tab>
+      //      <Tab value="Tutorial" label="Tutorial"></Tab>
+      //      <Tab value="Generate/Visualize" label="Generate/Visualize" ></Tab>
+      //      <Tab value="Pattern Match" label="Pattern Matching" ></Tab>
+      //      <Tab value="Wheeler Property" label="Find Ordering" ></Tab>
+       // </Tabs>
+      //</Box>
+    //); 
 
 } export default NavBar;
