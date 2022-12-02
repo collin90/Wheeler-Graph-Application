d887274dec8fc98e917c320c258ea12a8a8bee7c
Julia Ross <junioross12@gmail.com>
2022-12-02 09:12:15 -0500

    OILC to graph updated for up to 1 indegree for nodes. Hard coded for 'ACGT' alphabet. Need user to input alphabet? or scan L upon input

--- algorithms/graph_from_vectors.py
+++ algorithms/graph_from_vectors.py
@@ -1,7 +1,7 @@
-def graph_from_OILC(O, I, L, C):
+def graph_from_OILC(O, L, C):
 
     nodes_count = O.count("1")
-    edges = createEdges(I, O, L, C)
+    edges = createEdges(O, L, C)
     nodes = createNodes(nodes_count)
     return nodes, edges
 
@@ -11,19 +11,18 @@ def createNodes(count):
         nodes.append({'id':i})
     return nodes
 
-def createEdges(I, O, L, C):
+def createEdges(O, L, C):
     edges = []
-    edgeOG = findEdges(I, O, L, C)
+    edgeOG = findEdges(O, L, C)
     for source in edgeOG:
         for edge in edgeOG[source]:
             target = edge[0]
             label = edge[1]
             edges.append({'source': source, 'target': target, 'label': label, 'id':str(source)+"_"+str(target)})
     return edges
-def findEdges(I, O, L, C):
-    inie = incomingDict(I) # dictionary of counts of incoming edge capacity
-    edgy = {'A': 1, 'C': C[0], 'G': C[1], 'T': C[2]}
 
+def findEdges(O, L, C):
+    edgy = {'A': 1, 'C': C[0] + 1, 'G': C[1] + 1, 'T': C[2] + 1}
     edges = {} # running dictionary of 0 in-degree edges. key is the 0 in-degree node
     n = 1
     ed = 0
@@ -32,13 +31,7 @@ def findEdges(I, O, L, C):
             source = n
             label = L[ed]
             target = edgy[label] # dictionary of next node for a given label
-            print(edgy)
-            print(label)
-            print(inie)
-            print(target)
-            inie[target] = inie[target] - 1
-            if inie[target] < 1:
-                edgy[label] = edgy[label] + 1 # If we are out of incoming edges, then increment capacity for given label
+            edgy[label] = edgy[label] + 1 # If we are out of incoming edges, then increment capacity for given label
             if source not in edges:
                 edges[source] = []
             edges[source].append([target + 1, label])
@@ -47,36 +40,14 @@ def findEdges(I, O, L, C):
             n = n + 1 # Increment which node you're on
     return edges
 
-#Keep track of number of incoming edges for each node
-#nodes indexed starting at 1. Keys in the dict are node ids
-def incomingDict(I):
-    inie = {}
-    curr = 0
-    inie[0] = 0
-    for i in I:
-        if i == '1':
-            curr = curr + 1
-            inie[curr] = 0
-        else:
-            inie[curr] = inie[curr] + 1
-    return inie
+
 
 #TESTING 
-#print(L_to_BWT('TAAGCG', '011010101001'))
-#O = '01010010101011010101'
-#I = '10010101010101010101'
-#L = 'TTCGGAAATA'
-#this is an example from the article: https://www.sciencedirect.com/science/article/pii/S0304397517305285#fg0010
-#more edges than nodes makes things more complicated- subtract from number of nodes to get correct c matrix
-#this example might be more complicated than what we'd do?
-#doesn't work because to many edges 
-#O2 = '000100101100100100101'
-#L2 = 'AACAGCAGCGCGA'
-#I2 = '101001001001001001001'
-#C2 = [5, 9, 13, 0]
-#C = [4, 5, 7, 10]
-#print(graph_from_OILC(O, I, L, C))
-#print(incomingDict(I))
-#node_count = O2.count("1")
-#print(findEdges(I2, O2, L2, C2))
-#print(createEdges(I2, O2, L2, C2))
+
+O = '01001010101101101'
+#I = ''
+L = 'TCGGTTAA'
+C = [2, 3, 5, 8]
+
+#print(findEdges(O, L, C))
+#print(createEdges(O, L, C))
